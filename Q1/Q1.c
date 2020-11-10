9a52e735dd8dad0a82d6ab70ac861e834518977f
Pedro Cunha <pvcunh4@gmail.com>
2020-11-10 00:25:33 -0300

    Q1 comentada

--- Q1/Q1.c
+++ Q1/Q1.c
@@ -5,21 +5,37 @@
 #define TOTAL 100000
 #define NUM_THREADS 5
 
-
+//contador geral
 int contador = 0;
+
+//mutex para região crítica
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//flag para quando chegue a 10e6
 int flag = 0;
 
+//rotina para as threads
 void *inc(void *thread_id){
     int *id = (int *)thread_id;
+
+    // imprime quando uma thread começa a funcionar
     printf("Contando. Thread: %d\n", *id);
+
+    // executa um somatório enquanto não chegar no total
     while(!flag) {
+
+        // trava as outras threads para somente uma acessar a região crítica por vez
+
         pthread_mutex_lock(&mutex);
+
+        //faz as verificações e em seguida incrementa o contador
         if(!flag && contador == TOTAL) {
             printf("Contador alcançou %d na thread %d", TOTAL, *id);
             flag = 1;
         }
         contador++;
+
+        // desbloqueia para as outras threads
         pthread_mutex_unlock(&mutex);
     }
     pthread_exit(NULL);
@@ -34,6 +50,7 @@ int main() {
     
     scanf("%d", &n);
     
+    //cria threads
     for(i = 0; i < n; i++) {
         thread_ids[i] = (int *)malloc(sizeof(int));
         *thread_ids[i] = i;
@@ -44,6 +61,7 @@ int main() {
         }
     }
 
+    //junta todas as threads
     for(i = 0; i < n; i++)
         pthread_join(threads[i], NULL);
     
